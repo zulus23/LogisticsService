9dee763b7fd4ea42ad098a03e849292fb7e3f282
zulus23 <zulus23@gmail.com>
2016-08-04 21:50:18 +0300

    setup kendoui and webjar

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,12 +4,17 @@ import play.mvc.*;
 
 import views.html.*;
 
+import javax.inject.Inject;
+
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
 
+
+    @Inject
+    WebJarAssets webJarAssets;
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
@@ -17,7 +22,7 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is ready.",webJarAssets));
     }
 
 }
--- build.sbt
+++ build.sbt
@@ -2,12 +2,15 @@ name := "play-java"
 
 version := "1.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayJava)
+lazy val root = (project in file(".")).enablePlugins(PlayJava,PlayEbean)
 
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   javaJdbc,
   cache,
-  javaWs
+  javaWs,
+  "org.webjars" %% "webjars-play" % "2.5.0",
+  "org.webjars" % "bootstrap" % "3.3.7",
+  "org.webjars" % "jquery" % "2.2.4"
 )
