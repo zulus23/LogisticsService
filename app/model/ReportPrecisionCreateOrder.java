0953010de48e6e8bd8a2dc48db238385a332fd75
zulus23 <zulus23@gmail.com>
2016-08-05 16:34:33 +0300

    add model report 1

---
+++ app/model/ReportPrecisionCreateOrder.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Gukov on 05.08.2016.
+ */
+public class ReportPrecisionCreateOrder {
+
+    private String site;
+    private String orderNumber;
+    private String orderLine;
+    private String customer;
+    private String manager;
+    private String item;
+    private String itemDescription;
+    private LocalDate monthActualShip;
+    private LocalDate dataCreateOrder;
+    private LocalDate dataBeginProduction;
+    private int deviation;
+    private String deviationReason;
+    private String statusOrder;
+
+
+
+
+}
